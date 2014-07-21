f7d77bb00bead1bca01f937967f8fa766a0b7fc8
Karen Etheridge <ether@cpan.org>
2014-07-21 07:51:27 -0700

    add keywords to distribution metadata

--- lib/MooseX/Types/DateTime/MoreCoercions.pm
+++ lib/MooseX/Types/DateTime/MoreCoercions.pm
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 package MooseX::Types::DateTime::MoreCoercions;
 # ABSTRACT: Extensions to L<MooseX::Types::DateTime>
+# KEYWORDS: moose types constraints coercions date time datetime timestamp
 
 use Moose 0.41 ();
 use DateTime 0.4302 ();
